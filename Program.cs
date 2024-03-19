e8a7b1269609699ee221fd514f0aba1e9f01be79
FCalic07 <97953488+FCalic07@users.noreply.github.com>
2024-03-19 19:01:23 +0100

    Update Program.cs
    aaa

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Gym\n");
+            Console.WriteLine("Hrvatska");
         }
     }
 }
